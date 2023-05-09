565ea4a83c4b93b73c744cfefd6c0184c0ab473f
joseb <thompsonjose3@gmail.com>
2023-05-09 17:30:27 -0500

    Added stylings to link box

--- src/components/SignIn.js
+++ src/components/SignIn.js
@@ -2,10 +2,21 @@ import React from 'react';
 
 export function SignIn() {
   return (
-    <div class="bg-red-300 w-[300px] absolute top-[10rem] right-[70px] py-24 sm:py-32 flex flex-col  items-center">
-    <button type="submit" class="inline-flex rounded-lg text-sm font-semibold py-2.5 px-4 bg-slate-900 text-white hover:bg-slate-700 w-[90px] mt-0 sm:mt-0"><span>Sign in to account</span></button>
-    <button type="submit" class="inline-flex rounded-lg text-sm font-semibold py-2.5 px-4 bg-slate-900 text-white hover:bg-slate-700 w-[90px] mt-0 sm:mt-0"><span>Sign in to account</span></button>
-</div>
-
-  )
+    <div id="linkDiv" class="bg-red-300 w-[300px] absolute top-[10rem] right-[70px] p-4 h-[350px] !important">
+      <div class="bg-black w-full h-full">
+        <div class="flex flex-col items-start space-y-1 bg-green-300 w-full h-full p-4">
+        <a href=" " class="flex flex-row text-sm mb-0.875 w-full h-14 font-medium leading-none text-white bg-blue-900 hover:bg-gray-200 border border-gray-300 py-2 px-4 rounded">
+          <img src="~/assets/facebook.png" alt="Facebook Icon" width="12" height="12" />
+            Continue with Facebook
+          </a>
+          <a href=" " class="text-sm mb-0.875 w-full h-14 font-medium leading-none text-black bg-amazon hover:bg-gray-200 border border-gray-300 py-2 px-4 rounded">
+            Continue with Amazon
+          </a> 
+          <a href=" " class="text-sm mb-0.875 w-full h-14 font-medium leading-none text-white bg-email hover:bg-gray-200 border border-gray-300 py-2 px-4 rounded">
+            Continue with Email
+          </a>
+        </div>
+      </div>
+    </div>
+  );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@
 module.exports = { 
   purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'email': '#382110',
+        'amazon' : '#f5d47a;'
+      },
+    },
   },
   plugins: [],
 }
