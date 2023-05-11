d8763a1604f622462520e947d5c4b2188f9be779
Jose Avalos-Thompson <thompsonjose3@gmail.com>
2023-05-10 22:17:48 -0500

    Added Sign in styling

--- src/components/SignIn.js
+++ src/components/SignIn.js
@@ -1,7 +1,7 @@
 import React from 'react';
 export function SignIn() {
   return (
-    <div id="linkDiv" class="bg-white rounded-lg border border-solid border-gray-400 w-[350px] absolute top-[10rem] right-[70px] p-4 h-[350px] !important">
+    <div id="linkDiv" class="bg-white rounded-lg border border-solid border-gray-400 w-[350px] absolute top-[10rem] right-[70px] p-4 min-h-[350px] !important">
       <div class="bg-black w-full h-full">
         <div id = "linkDivBox" class="flex flex-col font-sans items-start space-y-1 bg-white w-full h-full p-4">
           <a id = "button-facebook" href=" " class="flex items-center  justify-between text-sm mb-0.875 w-full h-14 font-medium leading-none text-white bg-facebook hover:bg-gray-200 border border-gray-300 py-2 px-4 rounded">
@@ -16,12 +16,17 @@ export function SignIn() {
             <span class = " flex items-center justify-center ">Sign up with email</span>
           </a>
         </div>
-        <div>
-          <span>Already a member?<a href = " ">Sign In</a></span>
+        <div id = "sign-in" class = "p-4 bg-white">
+          <div class = "p-4  text-sm w-full h-full">
+            <span class = "items-center flex justify-around ">
+              Already a member?
+              <a class = "text-lg text-bold" href = " ">Sign In</a>
+            </span> 
+          </div>
         </div>
       </div>
     </div>
   );
 }
-
+//single sign-in/ oauth2
 
